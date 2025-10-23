0fb36daf3f0315ac482918afbc0568b019c75f93
Jeremy Smith <jeremy-smith@macolighting.com>
2025-10-23 11:07:15 +1000

    Update LaravelJsLocalizationServiceProvider.php

--- src/Mariuzzo/LaravelJsLocalization/LaravelJsLocalizationServiceProvider.php
+++ src/Mariuzzo/LaravelJsLocalization/LaravelJsLocalizationServiceProvider.php
@@ -71,13 +71,15 @@ class LaravelJsLocalizationServiceProvider extends ServiceProvider
             $translator = $app->make('Illuminate\Translation\Translator');
             $files = $app['files'];
 
-            if ($laravelMajorVersion === 4) {
-                $langs = $app['path.base'].'/app/lang';
-            } elseif ($laravelMajorVersion >= 9) {
-                $langs = $app['path.base'].'/lang';
-            } elseif ($laravelMajorVersion >= 5) {
-                $langs = $app['path.base'].'/resources/lang';
-            }
+            // We always use resources/lang :)
+            // if ($laravelMajorVersion === 4) {
+            //     $langs = $app['path.base'].'/app/lang';
+            // } elseif ($laravelMajorVersion >= 9) {
+            //     $langs = $app['path.base'].'/lang';
+            // } elseif ($laravelMajorVersion >= 5) {
+            //     $langs = $app['path.base'].'/resources/lang';
+            // }
+            $langs = $app['path.base'].'/resources/lang';
             $messages = $app['config']->get('localization-js.messages', []);
             $packagesLanguages = $translator->getLoader()->namespaces();
             $generator = new Generators\LangJsGenerator($files, $langs, $messages, $packagesLanguages);
